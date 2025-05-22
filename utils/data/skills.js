04c8189318a6d50a3717a5ff4ed00f9efb37f80b
fnnetwork <fnnetwork272@gmail.com>
2025-05-22 20:20:53 +0530

    Update skills.js

--- utils/data/skills.js
+++ utils/data/skills.js
@@ -3,22 +3,13 @@ export const skillsData = [
   'CSS',
   'Javascript',
   'Typescript',
-  'React',
-  'Next JS',
-  'Tailwind',
   'MongoDB',
-  'MySQL',
-  'PostgreSQL',
+  'Python',
   'Git',
   'AWS',
   'Bootstrap',
   'Docker',
-  'Go',
-  'Figma',
   'Firebase',
-  'MaterialUI',
-  'Nginx',
-  'Strapi'
 ]
 
 // Choose your skills from below. Make sure it's in the same format and spelled correctly.
