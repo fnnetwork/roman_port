cfbb88461ff0a807d7e96d179bed299785c0d887
fnnetwork <fnnetwork272@gmail.com>
2025-05-22 19:49:22 +0530

    Update contactsData.js

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,11 +1,5 @@
 export const contactsData = {
-    email: 'abusaid7388@gmail.com',
-    phone: '+8801608797655',
-    address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
-    github: 'https://github.com/said7388',
-    facebook: 'https://www.facebook.com/abusaid.riyaz/',
-    linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
-    twitter: 'https://twitter.com/said7388',
-    stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
-    devUsername: "said7388"
+    email: 'gauravmantoman1@gmail.com',
+    phone: '+916302525090',
+    address: 'KK POLYTECHNIC COLLEGE NERO MORE , GOVINDPUR , DHANBAD 828109. ',
 }
